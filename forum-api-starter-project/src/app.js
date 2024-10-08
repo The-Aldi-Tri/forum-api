db765a403ba2c43922f8bb6303fce86d7514cb5b
Aldi Tri <the.aldi.tri@gmail.com>
2024-10-08 03:46:57 +0000

    fix starter project setup

--- forum-api-starter-project/src/app.js
+++ forum-api-starter-project/src/app.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const createServer = require('./Infrastructures/http/createServer');
-const container = require('./Infrastructures/container');
+require("./Commons/setupEnv");
+
+const createServer = require("./Infrastructures/http/createServer");
+const container = require("./Infrastructures/container");
 
 (async () => {
   const server = await createServer(container);
